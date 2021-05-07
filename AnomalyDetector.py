2234019a50d86378b3ad2ddc4bcf3f15bbee728b
Liad Kehila <liad3131@gmail.com>
2021-05-07 13:23:14 +0300

    finished the anomaly detection, haven't tested yet

--- AnomalyDetector.py
+++ AnomalyDetector.py
@@ -2,34 +2,45 @@ import clr
 import os
 
 from AnomalyDetection import SimpleAnomalyDetector,HybridAnomalyDetector, TimeSeries
-
-
+import pandas as pd
+import tempfile
 class AnomalyDetector():
-    def __init__(self, model, csvPath):
-        model.learnNormal(TimeSeries(csvPath))
-        self.Model = model
-        with open(csvPath) as file:
-            self.VariablesChecked = file.readline()
+    def __init__(self, model, json):
+        df=pd.read_json(json)
+        with tempfile.NamedTemporaryFile() as tmp:
+            tmp.write(df.to_csv(index=False).encode())
+            tmp.seek(0)
+            model.learnNormal(TimeSeries(tmp.name))
+            self.Model = model
 
-    def getAnomalySpan(self, path):
-        arr = self.Model.detect(TimeSeries(path))
+    def getAnomalySpan(self, json):
+        df=pd.read_json(json)
+        with tempfile.NamedTemporaryFile() as tmp:
+            tmp.write(df.to_csv(index=False).encode())
+            tmp.seek(0)
+            arr = self.Model.detect(TimeSeries(tmp.name))
         if not arr:
             return []
         last = arr[0].timestamp
         first = arr[0]
-        res = []
+        res = {}
         for i in range(1, len(arr) - 1):
             last += 1
             if last == arr[i].timestamp:
                 continue
             col1, col2 = first.description.split(",", 1)
-            res.append({"span": f"[{first.timestamp},{last}]", "first": col1, "second": col2})
+            # res.append({"span": f"[{first.timestamp},{last}]", "first": col1, "second": col2})
+            res[col1]=res.get(col1,[])+[f"[{first.timestamp}, {last}]"]
+            res[col2]=res.get(col2,[])+[f"[{first.timestamp}, {last}]"]
             first = arr[i]
             last = arr[i].timestamp
         last += 2
         col1, col2 = first.description.split(",", 1)
-        res.append({"span": f"[{first.timestamp},{last}", "first": col1, "second": col2})
+        res[col1] = res.get(col1, []) + [f"[{first.timestamp}, {last}]"]
+        res[col2] = res.get(col2, []) + [f"[{first.timestamp}, {last}]"]
         return res
+    def getCorrelatedFeatures(self):
+        return self.Model.correlatedFeatures
 
 
 class LinearAnomalyDetector(AnomalyDetector):
