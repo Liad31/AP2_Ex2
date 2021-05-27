0d8f949b7c616c8c167ef6e2bd8090b95e3d6625
Tomer Royf <tomer12901@gmail.com>
2021-05-27 22:37:42 +0300

    after pull

--- static/main.js
+++ static/main.js
@@ -270,7 +270,30 @@ var waitinglist = [];
               dataType: "json",
               contentType: 'application/json;charset=UTF-8',
               accept: 'application/json;charset=UTF-8',
-              data: JSON.stringify({"predict_data":  json })
+              data: JSON.stringify({"predict_data":  json }),
+              success: function(data)
+                {
+                    alert("success");
+                },
+                error: function (jqXHR, exception) {
+                    var msg = '';
+                    if (jqXHR.status === 0) {
+                        msg = 'Not connect.\n Verify Network.';
+                    } else if (jqXHR.status == 404) {
+                        msg = 'Requested page not found. [404]';
+                    } else if (jqXHR.status == 500) {
+                        msg = 'Internal Server Error [500].';
+                    } else if (exception === 'parsererror') {
+                        msg = 'Requested JSON parse failed.';
+                    } else if (exception === 'timeout') {
+                        msg = 'Time out error.';
+                    } else if (exception === 'abort') {
+                        msg = 'Ajax request aborted.';
+                    } else {
+                        msg = 'Uncaught Error.\n' + jqXHR.responseText;
+                    }
+                    alert(msg);
+                },
             });
         }
     }
